0856c0dcdb4e5e5e9cce8f3c8c3eceb0e08a7906
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2015-05-20 16:04:31 +0530

    Migrate Events and set default root

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150520102850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "start_time"
+    t.integer  "duration"
+    t.string   "venue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
